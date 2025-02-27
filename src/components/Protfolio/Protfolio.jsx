579fdc126f6c26fd027e1d40a377cacbdf903df8
md shah jalal <shahjalal050505@gmail.com>
2025-02-27 14:13:49 +0600

    pricing part completed

--- src/components/Protfolio/Protfolio.jsx
+++ src/components/Protfolio/Protfolio.jsx
@@ -5,17 +5,32 @@ import Menu from "./Menu";
 const Protfolio = () => {
   const [items, setItems] = useState(Menu);
 
+  const filterItems = (category) => {
+    const updateItems = Menu.filter((currentItem) => {
+      return currentItem.category === category;
+    });
+    setItems(updateItems);
+  };
+
   return (
     <section className="work container section" id="work">
       <h2 className="section_title">Recent Works</h2>
       <div className="work_filters">
-        <span className="work_item">Everything</span>
-        <span onClick={() => filterItem("Creative")} className="work_item">
+        <span onClick={() => setItems(Menu)} className="work_item">
+          Everything
+        </span>
+        <span onClick={() => filterItems("Creative")} className="work_item">
           Creative
         </span>
-        <span className="work_item">Art</span>
-        <span className="work_item">Design</span>
-        <span className="work_item">Branding</span>
+        <span onClick={() => filterItems("Art")} className="work_item">
+          Art
+        </span>
+        <span onClick={() => filterItems("Design")} className="work_item">
+          Design
+        </span>
+        <span onClick={() => filterItems("Branding")} className="work_item">
+          Branding
+        </span>
       </div>
 
       <div className="work_container grid">
