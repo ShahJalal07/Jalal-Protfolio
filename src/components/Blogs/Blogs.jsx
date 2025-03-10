82a1847dc2b0760000a02a671eb613958c92cfe1
md shah jalal <shahjalal050505@gmail.com>
2025-03-10 14:24:29 +0600

    completed

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,8 +1,74 @@
 import React from "react";
 import "./blog.css";
+import Image1 from "../../assets/blog-1.svg";
+import Image2 from "../../assets/blog-2.svg";
+import Image3 from "../../assets/blog-3.svg";
 
 const Blogs = () => {
-  return <div>Blogs</div>;
+  return (
+    <section className="blog container section" id="blog">
+      <h2 className="section_title">Latest Posts</h2>
+
+      <div className="blog_container grid">
+        <div className="blog_card">
+          <div className="blog_thumb">
+            <a href="#">
+              <span className="blog_category">Review</span>
+              <a href="#">
+                <img src={Image1} alt="" className="blog_img" />
+              </a>
+            </a>
+          </div>
+          <div className="blog_details">
+            <h3 className="blog_title">5 best tools for React developers</h3>
+            <div className="blog_meta">
+              <span className="">02 May 2022</span>
+              <span className="blog_dot">.</span>
+              <span>Blogby</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="blog_card">
+          <div className="blog_thumb">
+            <a href="#">
+              <span className="blog_category">Tutorials</span>
+              <a href="#">
+                <img src={Image2} alt="" className="blog_img" />
+              </a>
+            </a>
+          </div>
+          <div className="blog_details">
+            <h3 className="blog_title">How to become a frontend developer</h3>
+            <div className="blog_meta">
+              <span className="">02 May 2022</span>
+              <span className="blog_dot">.</span>
+              <span>Blogby</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="blog_card">
+          <div className="blog_thumb">
+            <a href="#">
+              <span className="blog_category">Todo List</span>
+              <a href="#">
+                <img src={Image3} alt="" className="blog_img" />
+              </a>
+            </a>
+          </div>
+          <div className="blog_details">
+            <h3 className="blog_title">Use full Todo List</h3>
+            <div className="blog_meta">
+              <span className=""> 02 June 2022</span>
+              <span className="blog_dot">.</span>
+              <span>Blogby</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Blogs;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,57 @@ import React from "react";
 import "./contact.css";
 
 export const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="contact container section" id="contact">
+      <h2 className="section_title">Get In Touch</h2>
+
+      <div className="contact_container grid">
+        <div className="contact_info">
+          <h3 className="contact_title">Let's Talk About Everything!</h3>
+          <p className="contact_details">Don't like forms? Send me an Email.</p>
+        </div>
+
+        <form action="" className="contact_form">
+          <div className="contact_form-group">
+            <div className="contact_from-div">
+              <input
+                type="text"
+                className="contact_from-input"
+                placeholder="Insert your name"
+              />
+            </div>
+
+            <div className="contact_from-div">
+              <input
+                type="Email"
+                className="contact_from-input"
+                placeholder="Insert your Email"
+              />
+            </div>
+          </div>
+
+          <div className="contact_from-div">
+            <input
+              type="text"
+              className="contact_from-input"
+              placeholder="Insert your subject"
+            />
+          </div>
+          <div className="contact_from-div contact_from-area">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="20"
+              placeholder="Write your message"
+              className="contact_from-input "></textarea>
+          </div>
+
+          <button className="btn">Send Message</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
